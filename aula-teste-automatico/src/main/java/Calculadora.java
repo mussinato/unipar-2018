c6cdac3558e80e567533d9dbdc5e1a9f2eff8394
Renato Mussinato <mussinato@gmail.com>
2018-07-30 22:26:55 -0300

    Aula 30/07/2018: Tratamento de erros nos testes automáticos.

--- aula-teste-automatico/src/main/java/Calculadora.java
+++ aula-teste-automatico/src/main/java/Calculadora.java
@@ -1,7 +1,16 @@
 
 public class Calculadora {
 
-	public Double calcular(Double n1, Double n2, String op) {
+	public Double calcular(Double n1, Double n2, String op) throws Exception {
+		
+		if (n1 == null) {
+			n1 = 0.0;
+		}
+		
+		if (n2 == null) {
+			n2 = 0.0;
+		}
+		
 		double resultado = 0.0;
 		
 		if (op.equals("+")) {
@@ -9,6 +18,9 @@ public class Calculadora {
 		} else if (op.equals("-")) {
 			resultado = n1 - n2;
 		} else if (op.equals("/")) {
+			if (n2 == 0.0) {
+				throw new Exception("Não é possível dividir por zero.");
+			}
 			resultado = n1 / n2;
 		} else if (op.equals("*")) {
 			resultado = n1 * n2;
--- aula-teste-automatico/src/test/java/CalculadoraTest.java
+++ aula-teste-automatico/src/test/java/CalculadoraTest.java
@@ -4,28 +4,42 @@ import org.junit.Test;
 public class CalculadoraTest {
 
 	@Test
-	public void somarSucesso() {
+	public void somarSucesso() throws Exception {
 		Calculadora calc = new Calculadora();
 		Double result = calc.calcular(2.0, 3.0, "+");
 		Assert.assertEquals(5.0, result, 0.0);
 	}
 	
 	@Test
-	public void subtrairSucesso() {
+	public void somarValoresZero() throws Exception {
+		Calculadora calc = new Calculadora();
+		Double result = calc.calcular(0.0, 0.0, "+");
+		Assert.assertEquals(0.0, result, 0.0);
+	}
+	
+	@Test
+	public void somarValoresNulo() throws Exception {
+		Calculadora calc = new Calculadora();
+		Double result = calc.calcular(null, null, "+");
+		Assert.assertEquals(0.0, result, 0.0);
+	}
+	
+	@Test
+	public void subtrairSucesso() throws Exception {
 		Calculadora calc = new Calculadora();
 		Double result = calc.calcular(2.0, 3.0, "-");
 		Assert.assertEquals(-1.0, result, 0.0);
 	}
 	
 	@Test
-	public void multiplicarSucesso() {
+	public void multiplicarSucesso() throws Exception {
 		Calculadora calc = new Calculadora();
 		Double result = calc.calcular(2.0, 3.0, "*");
 		Assert.assertEquals(6.0, result, 0.0);
 	}
 	
 	@Test
-	public void dividirSucesso() {
+	public void dividirSucesso() throws Exception {
 		Calculadora calc = new Calculadora();
 		Double result = calc.calcular(10.0, 2.0, "/");
 		Assert.assertEquals(5.0, result, 0.0);
@@ -34,8 +48,13 @@ public class CalculadoraTest {
 	@Test
 	public void dividirPorZeroErro() {
 		Calculadora calc = new Calculadora();
-		double result = calc.calcular(1.0, 0.0, "/");
-		Assert.assertEquals(0.0, result, 0.0);
+		double result;
+		try {
+			result = calc.calcular(1.0, 0.0, "/");
+			Assert.fail();
+		} catch (Exception e) {
+
+		}
 	}
 	
 	
