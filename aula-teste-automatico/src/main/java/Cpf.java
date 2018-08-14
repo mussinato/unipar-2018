6f6c139eeac6145fa961dc2d63c2e9e12016d474
Renato Mussinato <mussinato@gmail.com>
2018-08-13 22:28:47 -0300

    Aula do dia 13/08/2018

--- aula-teste-automatico/src/main/java/Cpf.java
+++ aula-teste-automatico/src/main/java/Cpf.java
@@ -1,12 +1,30 @@
 import java.util.InputMismatchException;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Cpf {
 
 	public Boolean validar(String CPF) {
 		
-		CPF = CPF.replace(".", "").replace("-", "");
+		if (StringUtils.isBlank(CPF)) {
+			return false;
+		}
 		
-		if (CPF.length() < 11) {
+		CPF = CPF.replace(".", "").replace("-", "");
+
+		if (CPF.equals("11111111111") || CPF.equals("22222222222") ||
+				CPF.equals("33333333333") || CPF.equals("44444444444") ||
+				CPF.equals("55555555555") || CPF.equals("66666666666") ||
+				CPF.equals("77777777777") || CPF.equals("88888888888") ||
+				CPF.equals("99999999999") || CPF.equals("00000000000")) {
+			return false;
+		}
+
+		if (!StringUtils.isNumeric(CPF)) {
+			return false;
+		}
+
+		if (CPF.length() != 11) {
 			return false;
 		}
 		
--- aula-teste-automatico/src/test/java/CpfTest.java
+++ aula-teste-automatico/src/test/java/CpfTest.java
@@ -1,44 +1,91 @@
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CpfTest {
 	
+	private Cpf cpf = null;
+	
+	@Before
+	public void antes() {
+		cpf = new Cpf();
+	}
+	
+	@After
+	public void depois() {
+		
+	}
+	
 	@Test
 	public void cpfSucesso() {
-		Cpf cpf = new Cpf();
 		Boolean resultado = cpf.validar("12345678909");
 		Assert.assertEquals(true, resultado);
 	}
 	
 	@Test
 	public void cpfInvalido() {
-		Cpf cpf = new Cpf();
 		Boolean resultado = cpf.validar("12345678900");
 		Assert.assertEquals(false, resultado);
 	}
 	
 	@Test
 	public void cpfValidoComMascara() {
-		Cpf cpf = new Cpf();
 		Boolean resultado = cpf.validar("123.456.789-09");
 		Assert.assertEquals(true, resultado);
 	}
 	
 	@Test
 	public void cpfInvalidoComMascara() {
-		Cpf cpf = new Cpf();
 		Boolean resultado = cpf.validar("123.456.789-00");
 		Assert.assertEquals(false, resultado);
 	}
 	
 	@Test
 	public void cpfInvalidoMenor11Dig() {
-		Cpf cpf = new Cpf();
 		Boolean resultado = cpf.validar("123456789");
 		Assert.assertEquals(false, resultado);
 	}
 	
+	@Test
+	public void cpfMenor11DigComMascara() {
+		Boolean resultado = cpf.validar("075.887.109");
+		Assert.assertEquals(false, resultado);
+	}
 	
+	@Test
+	public void cpfMaior11Dig() {
+		Boolean resultado = cpf.validar("1234567890901");
+		Assert.assertEquals(false, resultado);
+	}
 	
+	@Test
+	public void cpfMaior11DigComMascara() {
+		Boolean resultado = cpf.validar("123.456.789-0901");
+		Assert.assertEquals(false, resultado);
+	}
 	
+	@Test
+	public void cpfNulo() {
+		Boolean resultado = cpf.validar(null);
+		Assert.assertEquals(false, resultado);
+	}
+	
+	@Test
+	public void cpfVazio() {
+		Boolean resultado = cpf.validar("");
+		Assert.assertEquals(false, resultado);
+	}
+	
+	@Test
+	public void cpfTodos1() {
+		Boolean resultado = cpf.validar("11111111111");
+		Assert.assertEquals(false, resultado);
+	}
+	
+	@Test
+	public void cpfComLetras() {
+		Boolean resultado = cpf.validar("aaabbbcccdd");
+		Assert.assertEquals(false, resultado);
+	}
 }
